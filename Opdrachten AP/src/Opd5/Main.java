de63652b423ec2947455da5069a5ba755e743de1
vlreinier <43336873+vlreinier@users.noreply.github.com>
2019-05-11 01:06:04 +0200

    Working on some stuff

--- Opdrachten AP/src/Opd5/Main.java
+++ Opdrachten AP/src/Opd5/Main.java
@@ -2,7 +2,7 @@ package Opd5;
 
 public class Main {
     public static void main(String[] arg) {
-        fibonacci_sequence(1);
+        fibonacci_sequence(5);
     }
 
     public static void fibonacci_sequence(int sequencelength) {
@@ -11,12 +11,10 @@ public class Main {
 
         if (sequencelength <= 0){
             System.out.println("Retry with higher numbers");
-        }else if(sequencelength == 1) {
-            System.out.println(first);
         }else {
             for (int i=0; i < sequencelength; i++){
                 int third = first + second;
-                System.out.println(first);
+                System.out.print(first+"  ");
                 first = second;
                 second = third;
             }
